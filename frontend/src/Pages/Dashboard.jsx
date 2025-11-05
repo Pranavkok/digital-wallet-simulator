a97fb575d57e0926376343b587c27ab84b77f541
yashsh13 <yashpat1302@gmail.com>
2025-11-05 21:14:56 +0530

    Removed unnecessary changes

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -109,7 +109,7 @@ function Dashboard() {
                     </div>
                     <button onClick={()=>{
                       navigate(`/send?id=${u._id}&name=${u.userName}`)
-                    }} className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors text-sm font-medium box-border">
+                    }} className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors text-sm font-medium">
                       Send Money
                     </button>
                   </div>
